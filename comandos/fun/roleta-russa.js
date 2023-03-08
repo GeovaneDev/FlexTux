bc5df4e974b80d8dadcf741d911d9d085d1397e2
Geovane <87013843+GeovaneDev@users.noreply.github.com>
2023-03-08 18:51:47 -0300

    Roleta Russa update

--- comandos/fun/roleta-russa.js
+++ comandos/fun/roleta-russa.js
@@ -6,16 +6,7 @@ module.exports = {
   type: Discord.ApplicationCommandType.ChatInput,
 
   run: async (client, interaction) => {
-    const { guild } = interaction;
-    const roleName = "Roleta Russa";
-
-    const role = guild.roles.cache.find((r) => r.name === roleName);
-    if (!role) {
-      return interaction.reply({
-        ephemeral: true,
-        content: `O cargo ${roleName} não existe neste servidor.`
-    });
-    }
+    
     const random = Math.floor(Math.random() * 6) + 1;
 
     if (random === 1) {
@@ -27,27 +18,12 @@ module.exports = {
         .setColor("Red");
       await interaction.reply({ embeds: [embed] });
 
-      const role = interaction.guild.roles.cache.find(
-        (r) => r.name === "Roleta Russa"
-      );
-      if (role) {
-        const member = interaction.member;
-        await member.roles.remove(role);
-      }
     } else {
       const embed = new Discord.EmbedBuilder()
         .setTitle("🔫 CLICK 🔫")
         .setDescription(`${interaction.user} sobreviveu à roleta-russa! 😅`)
         .setColor("Green");
       await interaction.reply({ embeds: [embed] });
-
-      const role = interaction.guild.roles.cache.find(
-        (r) => r.name === "Roleta Russa"
-      );
-      if (role) {
-        const member = interaction.member;
-        await member.roles.add(role);
-      }
     }
   },
 };
